2d1e693c13a21c7d6dd5461f34b593e61edc1d0c
Ali Mashhadi <axm2088@student.bham.ac.uk>
2024-03-19 11:32:23 +0000

    Add parameterisation mean, min, max test cases

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,7 +3,7 @@
 import pandas as pd
 import pandas.testing as pdt
 import datetime
-
+import pytest
 
 def test_daily_mean_zeros():
     """Test that mean function works for an array of zeros."""
@@ -49,3 +49,150 @@ def test_daily_mean_integers():
 
     # Need to use Pandas testing functions to compare arrays
     pdt.assert_frame_equal(daily_mean(test_input), test_result)
+
+@pytest.mark.parametrize(
+    "test_input, expected_output",
+    [
+        (
+            pd.DataFrame(
+                data=[ [0.0, 0.0], [0.0, 0.0], [0.0, 0.0] ],
+                index=[ pd.to_datetime('2000-01-01 01:00'),
+                        pd.to_datetime('2000-01-01 02:00'),
+                        pd.to_datetime('2000-01-01 03:00') ],
+                columns=[ 'A', 'B' ]
+            ),
+            pd.DataFrame(
+               data=[ [0.0, 0.0] ],
+               index=[ datetime.date(2000, 1, 1) ],
+               columns=[ 'A', 'B' ]
+            )
+        ),
+        (
+            pd.DataFrame(
+                data=[ [1, 2], [3, 4], [5, 6] ],
+                index=[ pd.to_datetime('2000-01-01 01:00'),
+                        pd.to_datetime('2000-01-01 02:00'),
+                        pd.to_datetime('2000-01-01 03:00') ],
+                columns=[ 'A', 'B' ],
+            ),
+            pd.DataFrame(
+                data=[ [3.0, 4.0] ],
+                index=[ datetime.date(2000, 1, 1) ],
+                columns=[ 'A', 'B' ]
+            )
+        ),
+    ])
+def test_daily_mean(test_input, expected_output):
+    """Test mean function works for array of zeroes and positive integers."""
+    from catchment.models import daily_mean
+    pdt.assert_frame_equal(daily_mean(test_input), expected_output)
+def test_daily_min_python_list():
+    """Test for AttributionError when passing a python list"""
+    from catchment.models import daily_min
+
+    with pytest.raises(AttributeError):
+        error_expected = daily_min([[3, 4, 7], [-3, 0, 5]])
+
+@pytest.mark.parametrize(
+    "test_input, expected_output",
+    [
+        (
+            pd.DataFrame(
+                data = [ [0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0] ],
+                index = [ pd.to_datetime('2000-01-01 01:00'),
+                        pd.to_datetime('2000-01-01 02:00'),
+                        pd.to_datetime('2000-01-01 03:00') ],
+                columns = [ 'A', 'B', 'C' ]
+            ),
+            pd.DataFrame(
+                data = [ [0.0, 0.0, 0.0] ],
+                index = [ datetime.date(2000, 1, 1) ],
+                columns = [ 'A', 'B', 'C' ]
+            )
+        ),
+        (
+            pd.DataFrame(
+                data = [ [4, 2, 5], [1, 6, 2], [4, 1, 9] ],
+                index = [ pd.to_datetime('2000-01-01 01:00'),
+                        pd.to_datetime('2000-01-01 02:00'),
+                        pd.to_datetime('2000-01-01 03:00') ],
+                columns = [ 'A', 'B', 'C' ]
+            ),
+            pd.DataFrame(
+                data = [ [4, 6, 9] ],
+                index = [ datetime.date(2000, 1, 1) ],
+                columns = [ 'A', 'B', 'C' ]
+            )
+        ),
+        (
+            pd.DataFrame(
+                data = [ [4, -2, 5], [1, -6, 2], [-4, -1, 9] ],
+                index = [ pd.to_datetime('2000-01-01 01:00'),
+                         pd.to_datetime('2000-01-01 02:00'),
+                         pd.to_datetime('2000-01-01 03:00') ],
+                columns = [ 'A', 'B', 'C' ]
+            ),
+            pd.DataFrame(
+                data = [ [4, -1, 9] ],
+                index = [ datetime.date(2000, 1, 1) ],
+                columns = [ 'A', 'B', 'C' ]
+            )
+        ),
+    ])
+
+def test_daily_mean(test_input, expected_output):
+    """Test mean function works for array of zeroes and positive integers."""
+    from catchment.models import daily_mean
+    pdt.assert_frame_equal(daily_mean(test_input), expected_output)
+
+@pytest.mark.parametrize(
+    "test_input, expected_output",
+    [
+        (
+            pd.DataFrame(
+                data = [ [0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0] ],
+                index = [ pd.to_datetime('2000-01-01 01:00'),
+                        pd.to_datetime('2000-01-01 02:00'),
+                        pd.to_datetime('2000-01-01 03:00') ],
+                columns = [ 'A', 'B', 'C' ]
+            ),
+            pd.DataFrame(
+                data = [ [0.0, 0.0, 0.0] ],
+                index = [ datetime.date(2000, 1, 1) ],
+                columns = [ 'A', 'B', 'C' ]
+            )
+        ),
+        (
+            pd.DataFrame(
+                data = [ [4, 2, 5], [1, 6, 2], [4, 1, 9] ],
+                index = [ pd.to_datetime('2000-01-01 01:00'),
+                        pd.to_datetime('2000-01-01 02:00'),
+                        pd.to_datetime('2000-01-01 03:00') ],
+                columns = [ 'A', 'B', 'C' ]
+            ),
+            pd.DataFrame(
+                data = [ [1, 1, 2] ],
+                index = [ datetime.date(2000, 1, 1) ],
+                columns = [ 'A', 'B', 'C' ]
+            )
+        ),
+        (
+            pd.DataFrame(
+                data = [ [4, -2, 5], [1, -6, 2], [-4, -1, 9] ],
+                index = [ pd.to_datetime('2000-01-01 01:00'),
+                         pd.to_datetime('2000-01-01 02:00'),
+                         pd.to_datetime('2000-01-01 03:00') ],
+                columns = [ 'A', 'B', 'C' ]
+            ),
+            pd.DataFrame(
+                data = [ [-4, -6, 2] ],
+                index = [ datetime.date(2000, 1, 1) ],
+                columns = [ 'A', 'B', 'C' ]
+            )
+        ),
+    ])
+
+def test_daily_max(test_input, expected_output):
+    """Test max function works for array of zeroes and positive integers."""
+    from catchment.models import daily_max
+    pdt.assert_frame_equal(daily_max(test_input), expected_output)
